538038d34ec9c0c3e34b33507b2758ca4e61bfb0
zgc <coding_zgc@163.com>
2016-07-03 23:59:50 +0800

    加入测试

---
+++ src/test/java/cn/zgc/seckill/BaseTest.java
@@ -0,0 +1,17 @@
+package cn.zgc.seckill;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 配置Spring和junit的整合（spring-test加junit），junit启动时加载SpringIOC容器
+ * @author zgc
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//指定Spring的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml"})
+public class BaseTest {
+
+}
