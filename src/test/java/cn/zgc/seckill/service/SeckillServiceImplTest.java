9eb4536aed802fdad06611a655a5c8dc8a230169
zgc <919506309@qq.com>
2016-07-31 00:20:24 +0800

    finished service

---
+++ src/test/java/cn/zgc/seckill/service/SeckillServiceImplTest.java
@@ -0,0 +1,56 @@
+package cn.zgc.seckill.service;
+
+import java.util.List;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import cn.zgc.seckill.BaseTest;
+import cn.zgc.seckill.dto.Exposer;
+import cn.zgc.seckill.dto.SeckillExecution;
+import cn.zgc.seckill.entity.Seckill;
+import cn.zgc.seckill.exception.RepeatKillException;
+import cn.zgc.seckill.exception.SeckillCloseException;
+
+public class SeckillServiceImplTest extends BaseTest{
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private SeckillService seckillService;
+
+	@Test
+	public void testGetSeckillList() throws Exception {
+		List<Seckill> list = seckillService.getSeckillList();
+		logger.info("list={}", list);
+	}
+
+	@Test
+	public void testGetById() throws Exception {
+		long id = 1003;
+		Seckill seckill = seckillService.getById(id);
+		logger.info("seckill={}", seckill);
+	}
+
+	// 测试代码完整逻辑，注意可重复执行
+	@Test
+	public void testSeckillLogic() throws Exception {
+		long id = 1001;
+		Exposer exposer = seckillService.exportSeckillUrl(id);
+		if (exposer.isExposed()) {
+			logger.info("exposer={}", exposer);
+			long phone = 13631231234L;
+			String md5 = exposer.getMd5();
+			try {
+				SeckillExecution execution = seckillService.executeSeckill(id, phone, md5);
+				logger.info("execution={}", execution);
+			} catch (RepeatKillException e) {
+				logger.error(e.getMessage());
+			} catch (SeckillCloseException e) {
+				logger.error(e.getMessage());
+			}
+		} else {
+			// 秒杀未开启
+			logger.error("exposer={}", exposer);
+		}
+	}
+}
